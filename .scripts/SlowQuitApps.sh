9fe17b03b36469ff0ac8a90301edfa5da520d951
Lachlan Collins <1438799+Lockyc@users.noreply.github.com>
2022-12-09 11:35:18 +1000

    Update SlowQuitApps.sh

--- .scripts/SlowQuitApps.sh
+++ .scripts/SlowQuitApps.sh
@@ -1,4 +1,4 @@
 # Restart SlowQuitApps
-#  Unexpected malfunctioning of SQA https://github.com/dteoh/SlowQuitApps/issues/73
+# Unexpected malfunctioning of SQA https://github.com/dteoh/SlowQuitApps/issues/73
 killall SlowQuitApps
 open -a "SlowQuitApps"
